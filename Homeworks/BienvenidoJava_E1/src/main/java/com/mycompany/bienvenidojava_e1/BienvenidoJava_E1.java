cff6ef937b4f774c7355de78421a394da6ae8a8b
BranHnrqz <86216647+BranHnrqz@users.noreply.github.com>
2023-08-22 09:12:39 -0600

    Go

---
+++ Homeworks/BienvenidoJava_E1/src/main/java/com/mycompany/bienvenidojava_e1/BienvenidoJava_E1.java
@@ -0,0 +1,13 @@
+package com.mycompany.bienvenidojava_e1;
+import javax.swing.JOptionPane;
+
+/**
+ * @author BRANDON
+ */
+
+public class BienvenidoJava_E1 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido al Mundo de Java :)");
+    }
+}
