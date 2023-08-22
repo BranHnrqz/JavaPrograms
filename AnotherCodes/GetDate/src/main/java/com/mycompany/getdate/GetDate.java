079ae73a56950d5f33169fb945a48338df2c6aae
BranHnrqz <86216647+BranHnrqz@users.noreply.github.com>
2023-08-21 21:42:05 -0600

    GO

---
+++ AnotherCodes/GetDate/src/main/java/com/mycompany/getdate/GetDate.java
@@ -0,0 +1,13 @@
+package com.mycompany.getdate;
+import java.util.*;
+
+/**
+ * @author BRANDON
+ */
+public class GetDate {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, hoy es: ");
+        System.out.println(new Date());
+    }
+}
