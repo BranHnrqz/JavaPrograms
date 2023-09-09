f4114f5a34b6152e02f1770a1285cf49b1e8c287
BranHnrqz <86216647+BranHnrqz@users.noreply.github.com>
2023-09-08 19:40:04 -0600

    GO

---
+++ Homeworks/Circumference/src/main/java/com/mycompany/circumference/Circumference.java
@@ -0,0 +1,28 @@
+package com.mycompany.circumference;
+import javax.swing.JOptionPane;
+
+/**
+ * @author BRANDON
+ */
+public class Circumference {
+
+    public static void main(String[] args) {
+        String r; 
+        
+        double rCircle;
+        
+        //Resultados
+        double area, volume; 
+        
+        //Lectura
+        r = JOptionPane.showInputDialog("Escribe el Radio de su Circulo: ");
+     
+        //Convirtiendo String a Double:
+        rCircle = Double.parseDouble(r);
+        
+        area = Math.PI * Math.pow(rCircle, 2);
+        volume = (4.0/3.0) * Math.PI * Math.pow(rCircle, 3);
+        
+        JOptionPane.showMessageDialog(null, "Area: " + area + "\nVolumen: " + volume, "Resultados :)", JOptionPane.PLAIN_MESSAGE);
+    }
+}
