f48c65ce4f001e7779b22ccab0e8582d56d4127b
BranHnrqz <86216647+BranHnrqz@users.noreply.github.com>
2023-10-09 21:10:25 -0600

    hwCommit
    GO

---
+++ ClassGuideProducts/src/main/java/com/mycompany/classguideproducts/ClassGuideProducts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classguideproducts;
+
+/**
+ *
+ * @author BRANDON
+ */
+public class ClassGuideProducts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
